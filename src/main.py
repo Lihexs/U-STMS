e96a2cc2d91428ce6a55362f1178d37a981859f1
Lior <Liordda@gmail.com>
2023-06-07 14:13:33 +0300

    Add initial Pygame code for window, world, and camera views, and moving screen with mouse feature

--- src/main.py
+++ src/main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
+
+# Window view
+win_width = 800
+win_height = 600
+win = pygame.display.set_mode((win_width, win_height))
+
+# World view
+world_width = 3000
+world_height = 3000
+world = pygame.Surface((world_width, world_height))
+
+# Camera view
+camera = pygame.Rect(0, 0, win_width, win_height)
+
+# Background
+bg = pygame.image.load('background/Untitled.png')
+world.blit(bg,(0,0))
+
+# Mouse movement
+dragging = False
+prev_mouse_pos = (0, 0)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    camera.move_ip(5, 0)  # Move the camera 5 pixels to the right
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    dragging = True
+                    prev_mouse_pos = pygame.mouse.get_pos()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    dragging = False
+        if dragging:
+            mouse_pos = pygame.mouse.get_pos()
+            dx, dy = prev_mouse_pos[0] - mouse_pos[0] , prev_mouse_pos[1] - mouse_pos[1]
+            camera.move_ip(dx,dy)
+            prev_mouse_pos = mouse_pos
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        win.blit(world.subsurface(camera), (0, 0))
+        pygame.display.flip()
